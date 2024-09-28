fba2b4326505f9bdd33ca85e7486ae72c6b13641
Manoj Chintaparti <manojchintaparti@gmail.com>
2024-09-28 20:45:02 +0530

    Added superuser method and test cases

--- app/core/models.py
+++ app/core/models.py
@@ -13,8 +13,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        # Custom user model object
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -27,7 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserManager()
+    objects = UserManager() # Assign custom user manager object to custom user model
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'  # Field used for authentication
 
